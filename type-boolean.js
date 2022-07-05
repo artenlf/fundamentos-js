48e8088002e5a651c4d2c7bf26d9a81c3c4b1bc4
artenlf <felipe.arten@gmail.com>
2022-07-04 22:07:01 -0300

    exemplos da aula 1 (number, string e boolean)

---
+++ type-boolean.js
@@ -0,0 +1,20 @@
+// tipo booleano
+
+//comparando numeros
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); //false
+
+const terceiroNumero = 5;
+
+console.log(primeiroNumero === terceiroNumero); //true
+
+// comparando strings
+
+const texto1 = 'abcdefg'
+
+const texto2 = 'ABCDEFG'
+
+console.log(texto1 === texto2); // false por conta da diferenciação de caracteres
---
+++ type-string.js
@@ -0,0 +1,21 @@
+// tipo string
+
+const texto1 = 'Olá, mundo!'; // com aspas simples
+const texto2 = "Olá, mundo!"; // com aspas duplas
+const senha = 'senhaSuperSegura456';
+const stringDeNumeros = '123456'; // string com números
+
+const citacao = 'O Leo disse "Olá, mundo!" usando javascript' // utilizando aspas duplas para fazer uma citação
+
+console.log(citacao);
+
+// concatenação (+)
+
+const meuNome = 'Felipe';
+const idade = 32;
+
+const nomeIdade = 'Meu nome é: ' + meuNome + ', eu tenho ' + idade + ' de idade';
+
+console.log(nomeIdade);
+
+// template string ou template literal
